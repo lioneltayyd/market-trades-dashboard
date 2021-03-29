0f000b15ecf75e383fbc6bc38b09ace8b2d62f47
lionelcub <lionel0702@hotmail.com>
2021-03-30 00:33:53 +0800

    Updated files.

--- sh/run_heroku_setup.sh
+++ sh/run_heroku_setup.sh
@@ -3,6 +3,12 @@ mkdir -p logs/runtime;
 mkdir -p ~/.streamlit/;
 chmod -R 777 ~/.streamlit;
 
+# Create credentials file for Streamlit.
+echo "\
+[general]\n\
+email=\"\"\n\
+" > /root/.streamlit/credentials.toml
+
 # Create config file for Streamlit. 
 echo "\
 [server]\n\
